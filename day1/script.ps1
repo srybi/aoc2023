d0c440869a3bd612224fe94ae72a7843afa6407a
studrybist5760 <stefan.rybinski@stud.th-rosenheim.de>
2023-12-01 17:46:06 +0100

    add day1 part2

--- day1/script.ps1
+++ day1/script.ps1
@@ -1,6 +1,12 @@
 $inputText = Get-Content day1\input.txt
-
 $sum = 0
+#------ part 2
+$map = [ordered]@{one = "o1e";  two = "t2o"; three = "t3e"; four = "f4r"; five = "f5e"; six = "s6x"; seven = "s7n";  eight = "e8t"; nine = "n9e"}
+
+foreach ($i in $map.Keys) {
+    $inputText = $inputText -replace $i, $map[$i]
+}
+#------ 
 
 foreach($line in $inputText){
     $numbers = $line -replace "[^0-9]" , ''
